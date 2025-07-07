bd09438017611018e6b9199bfd86ae4a65aa01e4
jaehyun0620 <zzz6451@naver.com>
2025-07-07 17:40:35 +0900

    초기 환경 세팅

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,14 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
+  {
+    "rules": {
+      "prefer-const": "warn", // 변수가 재할당 되지 않는다면 let 대신 const 사용하도록 경고
+      "no-redeclare": 1, // 변수 중복 선언시 경고
+      // 기타 룰 추가
+    },
+  },
 ];
 
 export default eslintConfig;
