d08c56c3bbf8d5caed9eb6f859825f769a6b7318
ji-yeoni <wldua16354@naver.com>
2025-07-11 13:10:22 +0900

    ✨ feat: 헤더 컴포넌트 UI 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+import Header from "@/components/layouts/Header/Header";
+
 export default function Home() {
   return (
+    <>
     <h1>Final-Project-oguogu v02</h1>
+    <Header cartItemCount={4} />
+    </>
   );
 }
