b9d48415d7858356d0b5b9e4a2e8f91e92360edd
최영준 <jjmullan24@gmail.com>
2025-07-14 14:53:57 +0900

    ✨ feat: Recommendation 컴포넌트 UI - 메인 타이틀, 서브 타이틀 변경 로직 - 개별 아이템 목록 UI 제작 후 최종 완성 예정 none

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import {
 } from '@/components/layouts/Category/Category';
 import Header from '@/components/layouts/Header/Header';
 import Navigation from '@/components/layouts/Navigation/Navigation';
+import { ProductRecommendation } from '@/components/layouts/Recommendation/Recommendation';
 
 export default async function Home() {
   return (
@@ -14,6 +15,11 @@ export default async function Home() {
       <ImageCategory />
       <TextCategory />
       <FilteredTextCategory />
+      <ProductRecommendation
+        title="온 가족이 함께 즐기는 텃밭 체험"
+        subTitle="7월 한 달 간 체험 상품 20% 할인!"
+        type="crop"
+      />
     </>
   );
 }
