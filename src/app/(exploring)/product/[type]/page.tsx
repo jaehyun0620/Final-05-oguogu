1d2fd3cceab493754bedbab688fcf82120bfb852
jaehyun0620 <zzz6451@naver.com>
2025-07-15 13:03:16 +0900

    ✨ feat: 상품 컴포넌트 기본 틀 구현

--- src/app/(exploring)/product/[type]/page.tsx
+++ src/app/(exploring)/product/[type]/page.tsx
@@ -1,10 +1,13 @@
+import CropItem from '@/components/elements/ProductItem/CropItem/CropItem';
+
 export default function ProductListByType() {
   return (
     <>
       <h1>1차 카테고리 상품 목록 페이지</h1>
-      <h2>crop : 농산물 상품</h2>
       <h2>experience : 체험 상품</h2>
       <h2>gardening : 텃밭 상품</h2>
+      <h2>crop : 농산물 상품</h2>
+      <CropItem />
     </>
   );
 }
