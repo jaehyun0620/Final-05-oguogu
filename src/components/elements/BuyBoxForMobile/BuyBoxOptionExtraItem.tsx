571e24648f2236a916af7da789ad84bd1a3c63c6
최영준 <jjmullan24@gmail.com>
2025-07-16 14:23:34 +0900

    💄 style: 세부 스타일 수정

--- src/components/elements/BuyBoxForMobile/BuyBoxOptionExtraItem.tsx
+++ src/components/elements/BuyBoxForMobile/BuyBoxOptionExtraItem.tsx
@@ -7,19 +7,23 @@ export default function BuyBoxOptionExtraItem({ date, time, count }: BuyBoxOptio
       {count > 0 ? (
         <label className="cursor-pointer">
           <input type="radio" name="timeOption" value={`${date} ${time}`} className="peer hidden" />
-          <div className="w-[140px] border peer-checked:border-oguogu-main-dark  border-oguogu-gray-2 rounded-lg p-3">
-            <p className="text-sm text-gray-800">{date}</p>
-            <p className="text-sm text-gray-500">{time}</p>
-            <p className="mt-2 text-black font-bold">{count} 자리 남음</p>
+          <div className="w-[100px] border peer-checked:border-oguogu-main-dark  border-oguogu-gray-2 rounded-lg p-3 flex flex-col gap-3">
+            <div className="flex flex-col gap-1">
+              <p className="text-xs text-oguogu-black">{date}</p>
+              <p className="text-xs text-oguogu-gray-4">{time}</p>
+            </div>
+            <p className="text-sm">{count} 자리 남음</p>
           </div>
         </label>
       ) : (
         <label className="cursor-not-allowed">
           <input type="radio" name="timeOption" value="2025-07-27-10" className="hidden" disabled />
-          <div className="w-[140px] border border-oguogu-gray-2 bg-oguogu-gray-1 rounded-lg p-3">
-            <p className="text-sm text-gray-800">{date}</p>
-            <p className="text-sm text-gray-500">{time}</p>
-            <p className="mt-2 text-black font-bold">품절</p>
+          <div className="w-[100px] border border-oguogu-gray-2 bg-oguogu-gray-1 rounded-lg p-3 flex flex-col gap-3">
+            <div className="flex flex-col gap-1">
+              <p className="text-xs text-oguogu-black">{date}</p>
+              <p className="text-xs text-oguogu-gray-4">{time}</p>
+            </div>
+            <p className="text-sm">품절</p>
           </div>
         </label>
       )}
