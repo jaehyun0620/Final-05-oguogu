21f43f3afdaad9cef5bc7c49d72aaba8c362c78a
jaehyun0620 <zzz6451@naver.com>
2025-07-17 10:31:03 +0900

    ✨ feat: 상품 상세 페이지 초기 작업

--- src/app/(exploring)/product/[type]/[category]/[_id]/detail/page.tsx
+++ src/app/(exploring)/product/[type]/[category]/[_id]/detail/page.tsx
@@ -1,5 +1,5 @@
 import BuyBox from '@/components/elements/BuyBox/BuyBox';
-import Title from '@/components/elements/CommonTitleItem/Title';
+// import Title from '@/components/elements/CommonTitleItem/Title';
 import { PathCaseOne } from '@/components/elements/Path/Path';
 import ProductLinkItem from '@/components/elements/ProductLink/ProductLink';
 import Image from 'next/image';
@@ -23,7 +23,7 @@ export default function ProductDetail() {
       <section className="px-4 pt-4">
         <section className="flex flex-col gap-4">
           <PathCaseOne title="옥수수" />
-          <Title title="쫀득쫀득 대학 미백 찰옥수수 30개입" content="올해 수확! 알갱이가 톡톡 터지는 맛있는 찰옥수수" />
+          {/* <Title title="쫀득쫀득 대학 미백 찰옥수수 30개입" content="올해 수확! 알갱이가 톡톡 터지는 맛있는 찰옥수수" /> */}
           {/* 가격 정보 */}
           <div>
             <s className="text-[16px] text-oguogu-gray-2">20,000원</s>
--- src/app/(exploring)/product/[type]/[category]/[_id]/layout.tsx
+++ src/app/(exploring)/product/[type]/[category]/[_id]/layout.tsx
@@ -1,17 +1,17 @@
-'use client';
-
 import { TextCategoryItem } from '@/components/elements/CategoryItem/CategoryItem';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
-      <div>헤더부분 </div>
-      <nav className="flex min-w-[320px] max-w-[768px]">
+      <body className="max-w-[320px] min-w-[320px] mx-auto">
+        {/* <div>헤더부분 </div> */}
+        {/* <nav className="flex min-w-[320px] max-w-[768px]"> */}
         <TextCategoryItem params="crop" subParams="veggie/1/detail" title="상품 정보" isClick={true} />
-        <TextCategoryItem params="crop" subParams="veggie/1/review" title="리뷰(1,280)" isClick={false} />
-        <TextCategoryItem params="crop" subParams="veggie/1/qna" title="문의(9,999)" isClick={false} />
-      </nav>
-      <body className="max-w-[320px] min-w-[320px] mx-auto">{children}</body>
+        {/* <TextCategoryItem params="crop" subParams="veggie/1/review" title="리뷰(1,280)" isClick={false} />
+          <TextCategoryItem params="crop" subParams="veggie/1/qna" title="문의(9,999)" isClick={false} /> */}
+        {/* </nav> */}
+        {children}
+      </body>
     </html>
   );
 }
